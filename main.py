923698c987c1536b5d769171ec00ca378356c36e
Vitalii Rybas <rybas.vitalii@kntu.edu.ua>
2025-08-01 14:26:32 +0300

    🛠 Switched to habits_v2.db to recreate schema

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 )
 
 # База даних
-SQLALCHEMY_DATABASE_URL = "sqlite:///./habits.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./habits_v2.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
