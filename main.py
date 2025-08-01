434b3d5bdf5c6e7d10adf520ff982338f97d2fcb
Vitalii Rybas <rybas.vitalii@kntu.edu.ua>
2025-08-01 14:35:03 +0300

    🚑 create new db to fix streak_data issue

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.add_middleware(
 # ========================
 # Підключення до БД
 # ========================
-SQLALCHEMY_DATABASE_URL = "sqlite:///./habits_v2.db"  # НОВА база
+Base.metadata.create_all(bind=engine)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
